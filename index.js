a85a5a7e6faf817a23c662c95c14875b0a9a00a7
benjamacmullen <benjamacsk8man@gmail.com>
2022-09-15 23:19:50 -0300

    bien

--- index.js
+++ index.js
@@ -14,22 +14,22 @@ imagen:"./assets/img/sauce.webp"},
 { id: 3, 
 nombre: "Maple-BJJ",
 precio: 2500,
-imagen: "../assets/img/bjj.jpeg"},
+imagen: "./assets/img/bjj.jpeg"},
 
 { id: 4, 
 nombre: "Guatambu-PISCIS", 
 precio: 2500,
-imagen: "../assets/img/gua piscids.webp"},
+imagen: "./assets/img/gua piscids.webp"},
 
 { id: 5, 
 nombre: "Guatambu-SAUCE", 
 precio: 2500,
-imagen: "../assets/img/gua saUCE.webp"},
+imagen: "./assets/img/gua saUCE.webp"},
 
 { id: 6, 
 nombre: "Guatambu-BJJ", 
 precio: 2500,
-imagen: "../assets/img/gua bjj.jpeg" },
+imagen: "./assets/img/gua bjj.jpeg" },
 
 ]
 
@@ -38,7 +38,7 @@ const dibujarProductos = ()=>{
     productos.forEach((producto,indice) => {
         let card= document.createElement("div");
         card.classList.add("card" , "col-sm-12" , "col-lg-3");
-        card.innerHTML= `<img src="${producto.nombre}" class="card-img-top"  alt="Card image cap">
+        card.innerHTML= `<img src="${producto.imagen}" class="card-img-top"  alt="Card image cap">
         <div class="card-body">
         <h5 class="card-title">${producto.nombre}</h5>
         <p class="card-text">${producto.precio}</p>
